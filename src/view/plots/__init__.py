b381a4bb3e2c75bd89c87592381f774da368912d
Alessandro Cecilio <alessandro.m.cecilio@gmail.com>
2026-06-08 09:37:57 -0300

    reestruturação do modúlo plots

--- src/view/plots/__init__.py
+++ src/view/plots/__init__.py
@@ -1,12 +1,18 @@
+from ._config import *
+from ._formatters import *
+
 from .qualidade import grafico_percentual_missing_data
-from .univariados_numericos import grafico_distribuicao_numerica
-from .bivariados import grafico_corr_scatter, grafico_dependencia_categorica
-from .univariados_categoricos import (
+
+from .univariados import (
+    grafico_distribuicao_numerica,
     grafico_metricas_categorias, 
     grafico_top_categorias, 
     grafico_top_tempo
 )
-from .bivariados_cat_vs_num import (
+
+from .bivariados import (
+    grafico_corr_scatter, 
+    grafico_dependencia_categorica,
     grafico_cat_vs_num_boxplot,    
     grafico_cat_vs_num_violinplot
 )
